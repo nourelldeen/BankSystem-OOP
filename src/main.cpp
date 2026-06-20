cc7a4dd69382303b37690cdaf48e40ce6b1b1e3c
Nour Eldeen <noureldeensayied@gmail.com>
2026-06-20 19:40:25 +0300

    Fix main.cpp: replace system pause with portable alternative

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "../include/clsLoginScreen.h"
+#include <iostream>
+
 int main()
 {
-	while(true)
-	{
-		if (!clsLoginScreen::ShowLoginScreen())
-			break;
-	}
-	system("pause>0");
-	return 0;
+    while (true)
+    {
+        if (!clsLoginScreen::ShowLoginScreen())
+            break;
+    }
+
+    std::cout << "\nPress Enter to exit...";
+    std::cin.get();
+    return 0;
 }
